c3244a1de67ac8116cede8a58fb32e1b6beb756e
Tyler Wray <wraytw@gmail.com>
2023-07-24 19:46:55 -0600

    Github Link

--- lib/scrum_poker_web/live/layout.ex
+++ lib/scrum_poker_web/live/layout.ex
@@ -7,11 +7,11 @@ defmodule ScrumPokerWeb.Layout do
 
   def anonymous_nav(assigns) do
     ~H"""
-    <nav class="grid sm:grid-cols-[1fr_auto] grid-cols-1 gap-4 items-center sm:justify-items-start justify-items-center p-6 max-w-6xl mx-auto">
+    <nav class="grid sm:grid-cols-[1fr_auto_auto] grid-cols-1 gap-4 items-center sm:justify-items-start justify-items-center p-6 max-w-6xl mx-auto">
       <.link
         role="menuitem"
         navigate="/signin"
-        class="outline-none px-2 py-1 flex gap-2 items-center rounded-lg hover text-gray-600 dark:text-gray-100 hover:bg-gray-600/10 dark:hover:bg-gray-300/20"
+        class="outline-none px-2 py-1 flex gap-2 items-center rounded-lg hover text-gray-800 dark:text-gray-100 hover:bg-gray-600/10 dark:hover:bg-gray-300/20"
       >
         <%!-- Spade icon --%>
         <svg
@@ -26,6 +26,26 @@ defmodule ScrumPokerWeb.Layout do
         </svg>
         <span>Sign in</span>
       </.link>
+      <.link
+        role="menuitem"
+        href="https://github.com/tylerwray/scrum_poker"
+        target="_blank"
+        class="outline-none px-2 py-1 flex gap-2 items-center rounded-lg hover text-gray-800 dark:text-gray-100 hover:bg-gray-600/10 dark:hover:bg-gray-300/20"
+      >
+        <svg
+          height="20"
+          aria-hidden="true"
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="20"
+          data-view-component="true"
+          class="fill-[#24292f] dark:fill-white"
+        >
+          <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z">
+          </path>
+        </svg>
+        Github <Heroicons.arrow_top_right_on_square class="w-4 h-4" />
+      </.link>
       <h2 class="text-lg"><%= @display_name %></h2>
     </nav>
     """
@@ -34,11 +54,11 @@ defmodule ScrumPokerWeb.Layout do
   def nav(assigns) do
     ~H"""
     <%= if @current_user do %>
-      <nav class="grid sm:grid-cols-[1fr_auto] grid-cols-1 gap-4 items-center sm:justify-items-start justify-items-center px-12 py-6 max-w-6xl mx-auto">
+      <nav class="grid sm:grid-cols-[1fr_auto_auto] grid-cols-1 gap-4 items-center sm:justify-items-start justify-items-center px-12 py-6 max-w-6xl mx-auto">
         <.link
           role="menuitem"
           navigate="/home"
-          class="outline-none px-2 py-1 flex gap-2 items-center rounded-lg hover text-gray-600 dark:text-gray-100 hover:bg-gray-600/10 dark:hover:bg-gray-300/20"
+          class="outline-none px-2 py-1 flex gap-2 items-center rounded-lg hover text-gray-800 dark:text-gray-100 hover:bg-gray-600/10 dark:hover:bg-gray-300/20"
         >
           <%!-- Spade icon --%>
           <svg
@@ -52,6 +72,26 @@ defmodule ScrumPokerWeb.Layout do
             <path d="M7.184 11.246A3.5 3.5 0 0 1 1 9c0-1.602 1.14-2.633 2.66-4.008C4.986 3.792 6.602 2.33 8 0c1.398 2.33 3.014 3.792 4.34 4.992C13.86 6.367 15 7.398 15 9a3.5 3.5 0 0 1-6.184 2.246 19.92 19.92 0 0 0 1.582 2.907c.231.35-.02.847-.438.847H6.04c-.419 0-.67-.497-.438-.847a19.919 19.919 0 0 0 1.582-2.907z" />
           </svg>
           <span>Home</span>
+        </.link>
+        <.link
+          role="menuitem"
+          href="https://github.com/tylerwray/scrum_poker"
+          target="_blank"
+          class="outline-none px-2 py-1 flex gap-2 items-center rounded-lg hover text-gray-800 dark:text-gray-100 hover:bg-gray-600/10 dark:hover:bg-gray-300/20"
+        >
+          <svg
+            height="20"
+            aria-hidden="true"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="20"
+            data-view-component="true"
+            class="fill-[#24292f] dark:fill-white"
+          >
+            <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z">
+            </path>
+          </svg>
+          Github <Heroicons.arrow_top_right_on_square class="w-4 h-4" />
         </.link>
         <.dropdown id="user-tile">
           <:img src={@current_user.avatar_url} />
